dc6b22257b2541df33b813c527b7217f2b3a627d
fransvanderlek <fransvanderlek@gmail.com>
2021-10-21 22:29:58 +0200

    Hello Demo Workplace!!

--- src/main/java/org/intelligentindustry/demoworkplace/MainApp.java
+++ src/main/java/org/intelligentindustry/demoworkplace/MainApp.java
@@ -1,11 +1,9 @@
 package org.intelligentindustry.demoworkplace;
 
-import org.apache.camel.main.Main;
-import org.intelligentindustry.opcua.remoteproxy.RemoteProxyRouteBuilder;
-
 public class MainApp {
 
 	public static void main(String... args) throws Exception {
+		System.out.println("Hello Demo Workplace!");
      
     }
 }
