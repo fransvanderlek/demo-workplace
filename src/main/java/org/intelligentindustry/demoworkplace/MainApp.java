0abc00d1495843298346a511d3921b57373403e6
fransvanderlek <fransvanderlek@gmail.com>
2021-10-23 14:47:40 +0200

    fixes

--- src/main/java/org/intelligentindustry/demoworkplace/MainApp.java
+++ src/main/java/org/intelligentindustry/demoworkplace/MainApp.java
@@ -1,5 +1,6 @@
 package org.intelligentindustry.demoworkplace;
 
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
 
 
 public class MainApp {
@@ -7,8 +8,9 @@ public class MainApp {
 	public static void main(String... args) throws Exception {
 		
 		System.out.println("Starting MainApp");
-		
- 
+
+		OpcUaClient client;	
+	
      
     }
 }
